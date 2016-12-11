4032a8a57133219d9e95c978db79c61fb77acf52
aannccaa <aannccaa@gmail.com>
2016-12-11 20:17:52 +0100

    loop python

---
+++ Lecture 3- Problem Solving_Loops.py
@@ -0,0 +1,25 @@
+# for name in "Anca", "Cata", "Serban":
+#     print name
+
+# for i in range(10):
+#     print i
+#help(range)
+
+# total = 0
+# for i in 5, 7, 11, 13:
+#     print(i)
+#     total = total + i
+
+# print(total)
+
+# for _ in range(10):
+#     print("Hello!")
+
+# for drink in "soda", "Cola":
+#     print("Would you like a " + drink + "?")
+
+# for i in range(10):
+#     turtle.forward(15)
+#     turtle.penup()
+#     turtle.forward(5)
+#     turtle.pendown()
